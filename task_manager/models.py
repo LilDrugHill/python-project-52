e257f04d8c92ec42eabec42ada1eb07e3231bb06
LilDrugHill <smookigrow@icloud.com>
2022-10-14 17:45:02 +0300

    all staps finished

---
+++ task_manager/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+
+
+
+class UserStr(User):
+    def __str__(self):
+        return self.get_full_name()
+
+    class Meta:
+        proxy = True
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from task_manager.statuses.models import StatusModel
-from django.contrib.auth.models import User
+from task_manager.models import UserStr
 from task_manager.labels.models import LabelModel
 
 
@@ -8,9 +8,9 @@ class TaskModel(models.Model):
     name = models.CharField(max_length=25)
     description = models.CharField(max_length=255)
     status = models.ForeignKey(StatusModel, on_delete=models.PROTECT)
-    author = models.ForeignKey(User, related_name="author", on_delete=models.PROTECT)
+    author = models.ForeignKey(UserStr, related_name="author", on_delete=models.PROTECT)
     executor = models.ForeignKey(
-        User, related_name="executor", on_delete=models.PROTECT
+        UserStr, related_name="executor", on_delete=models.PROTECT
     )
     created_at = models.DateTimeField(auto_now_add=True)
     labels = models.ManyToManyField(LabelModel, blank=True)
