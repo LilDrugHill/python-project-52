228449b6638171eeeb7c57b6cdfc0e118b60dc81
LilDrugHill <smookigrow@icloud.com>
2022-10-15 13:33:25 +0300

    i18n added ru complite

--- task_manager/views.py
+++ task_manager/views.py
@@ -33,7 +33,7 @@ class RegisterUser(DataMixin, SuccessMessageMixin, CreateView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        c_def = self.get_user_context(title="Reg page")
+        c_def = self.get_user_context(title=gettext('Registration page'))
         return dict(list(context.items()) + list(c_def.items()))
 
 
