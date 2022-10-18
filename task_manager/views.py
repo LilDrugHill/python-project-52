3ca3d6ae1b588cdd94bc8d86bba1824bddf264dc
LilDrugHill <smookigrow@icloud.com>
2022-10-18 17:37:39 +0300

    custom post with redirect(reverse_lazy) in RegisterUser

--- task_manager/views.py
+++ task_manager/views.py
@@ -32,7 +32,7 @@ class RegisterUser(DataMixin, SuccessMessageMixin, CreateView):
 
     def post(self, request, *args, **kwargs):
         super(RegisterUser, self).post(self, request, *args, **kwargs)
-        return redirect('')
+        return redirect(reverse_lazy('home'))
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
