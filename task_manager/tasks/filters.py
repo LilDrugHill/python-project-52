038b77855292c77729ce29aed2bbdc9d3c509651
LilDrugHill <smookigrow@icloud.com>
2022-10-21 14:26:55 +0300

    some changes

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -6,14 +6,16 @@ from task_manager.labels.models import LabelModel
 
 
 class TaskFilter(django_filters.FilterSet):
+    label = django_filters.ModelChoiceFilter(field_name='labels', queryset=LabelModel.objects.all(),
+                                             label=gettext('Label'))
+
     self_task = django_filters.BooleanFilter(
         method="show_self_task", widget=CheckboxInput(), label=gettext("Only own tasks")
     )
-    labels = django_filters.ModelChoiceFilter(field_name='labels', queryset=LabelModel.objects.all(), label=gettext('Label'))
 
     class Meta:
         model = TaskModel
-        fields = ("status", "executor", "labels")
+        fields = ("status", "executor")
 
     def show_self_task(self, queryset, name, value):
         if value is True:
