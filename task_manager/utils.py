c525ac502901f68e93f1705223596796c47bb161
LilDrugHill <smookigrow@icloud.com>
2022-10-31 03:18:03 +0300

    some possible fixed (server 500)

--- task_manager/utils.py
+++ task_manager/utils.py
@@ -14,7 +14,7 @@ class CustomLoginRequiredMixin(LoginRequiredMixin):
                 request, messages.WARNING, self.permission_denied_message
             )
             return self.handle_no_permission()
-        return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
 
 class CustomDispatchForDeletionMixin:
